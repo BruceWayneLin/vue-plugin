c01a8dd37f193c28baf87d414661c569f7c6a9f9
Skanda lin <anikiwayne@gmail.com>
2020-12-14 20:10:25 +0800

    updated

--- modules/gameFrame/index.js
+++ modules/gameFrame/index.js
@@ -3,6 +3,10 @@ import { events } from "../gameFrame/Events";
 import GameFrame from "../gameFrame/gameFrame.vue";
 var VueUi = require('@vue/ui')
 import '@vue/ui/dist/vue-ui.css'
+var lodash = require('lodash');
+var output = lodash.without([1, 2, 3], 1);
+console.log(output);
+
 class Installer { 
   constructor() { 
   }
